0b01d5c871479965ea443ee5af333c7aa53efcf1
hsqStephenZhang <stephenzhang666666@gmail.com>
2026-02-21 09:33:55 +0100

    feat: support x86 64

--- benches/atoi.rs
+++ benches/atoi.rs
@@ -3,7 +3,6 @@ use std::hint::black_box;
 
 use atoi_simd::parse as atoi_simd_parse;
 use criterion::{Criterion, criterion_group, criterion_main};
-use sonic_number_simd::{neon::simd_str2int_neon, sve::simd_str2int_sve2};
 
 const NUMBERS: &[&str] = &[
     "1",
@@ -30,17 +29,39 @@ fn bench_i64(c: &mut Criterion) {
         let bench_name_neon = format!("sonic-number neon len={}", i + 1);
         let bench_name_sve = format!("sonic-number sve len={}", i + 1);
         let bench_name_atoi = format!("atoi_simd parse len={}", i + 1);
+        let bench_name_sse = format!("sonic-number sse len={}", i + 1);
 
+        #[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
         c.bench_function(&bench_name_neon, |b| {
             b.iter(|| {
-                let val = unsafe { simd_str2int_neon(black_box(s.as_bytes()), s.len()) };
+                let val = unsafe {
+                    sonic_number_simd::neon::simd_str2int_neon(black_box(s.as_bytes()), s.len())
+                };
                 assert_eq!(val.0, expected);
             })
         });
 
+        #[cfg(all(target_arch = "aarch64", target_feature = "sve2"))]
         c.bench_function(&bench_name_sve, |b| {
             b.iter(|| {
-                let val = unsafe { simd_str2int_sve2(black_box(s.as_bytes()), s.len()) };
+                let val = unsafe {
+                    sonic_number_simd::sve::simd_str2int_sve2(black_box(s.as_bytes()), s.len())
+                };
+                assert_eq!(val.0, expected);
+            })
+        });
+
+        #[cfg(all(
+            target_arch = "x86_64",
+            target_feature = "pclmulqdq",
+            target_feature = "avx2",
+            target_feature = "sse2"
+        ))]
+        c.bench_function(&bench_name_sse, |b| {
+            b.iter(|| {
+                let val = unsafe {
+                    sonic_number_simd::x86_64::simd_str2int(black_box(s.as_bytes()), s.len())
+                };
                 assert_eq!(val.0, expected);
             })
         });
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
-#[cfg(all(target_arch = "aarch64", target_feature = "sve2"))]
-pub mod sve;
 #[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
 pub mod neon;
+#[cfg(all(target_arch = "aarch64", target_feature = "sve2"))]
+pub mod sve;
+
+#[cfg(all(
+    target_arch = "x86_64",
+    target_feature = "pclmulqdq",
+    target_feature = "avx2",
+    target_feature = "sse2"
+))]
+pub mod x86_64;
